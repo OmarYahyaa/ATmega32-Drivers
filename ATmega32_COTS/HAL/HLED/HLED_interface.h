461e83d8b38b7ad672fcb78f4561afce3b420fe4
OMAR YAHYA <OMAR YAHYA@MSI>
2023-07-19 14:34:56 +0200

    Added LED Driver

---
+++ ATmega32_COTS/HAL/HLED/HLED_interface.h
@@ -0,0 +1,25 @@
+/*
+ * HLED_interface.h
+ *
+ *  Created on: Jul 18, 2023
+ *      Author: OMAR YAHYA
+ */
+
+#ifndef HLED_HLED_INTERFACE_H_
+#define HLED_HLED_INTERFACE_H_
+
+typedef enum {
+	SOURCE = 0, SINK = 1
+} Connection_t;
+
+typedef struct {
+	u8 LED_Pin;
+	u8 LED_Connection;
+} Led_t;
+
+ErrorState_t HLED_enInit(Led_t *PtrLed);
+ErrorState_t HLED_enEnable(Led_t *PtrLed);
+ErrorState_t HLED_enDisable(Led_t *PtrLed);
+ErrorState_t HLED_enToggle(Led_t *PtrLed);
+
+#endif /* HLED_HLED_INTERFACE_H_ */
